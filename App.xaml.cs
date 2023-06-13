b4d71c80691066363683fd95e84a0b36d83c8c2b
Norman Aranez <araneznorman12@gmail.com>
2023-06-13 19:59:26 +0800

    initial

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ConnectMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ConnectMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
